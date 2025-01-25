0845f8fb875d7a93c5a73919580453523a258ed5
XDRONIN <akashprakashmattathil@gmail.com>
2025-01-25 14:56:05 +0530

    landing page base

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -1,13 +1,50 @@
 <template>
-  <nav class="w-100% bg-black text-white">
-    NAVBAR
-    <div class="text-blue-500">This holds the main nav</div>
-    <div>This holds the login button</div>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <nav
+    class="w-100% h-30 bg-black text-white flex items-center justify-between"
+  >
+    SolveChain
+    <div
+      class="text-blue-500 bg-white rounded-2xl flex items-center sm:w-120 sm:h-15 lg:w-200"
+    >
+      <span
+        class="w-30 bg-gray-400 rounded-2xl flex justify-center h-12 items-center m-1 flex-1"
+        >Home</span
+      >
+      <span
+        class="w-30 bg-gray-400 rounded-2xl flex justify-center h-12 items-center flex-1 m-1"
+        >Comunity</span
+      >
+      <span
+        class="w-30 bg-gray-400 rounded-2xl flex justify-center h-12 items-center flex-1 m-1"
+        >how it works</span
+      >
+      <span
+        class="w-30 bg-gray-400 rounded-2xl flex justify-center h-12 items-center flex-1 m-1"
+        >Documentation</span
+      >
+    </div>
+    <div><button>Sign In</button></div>
   </nav>
-  <main class="h-218 bg-amber-400">
-    MAIN COMPONENTS
-    <div>This div holds the text</div>
-    <div>This div holds the buttons</div>
-    <div>This holds the Cube</div>
+  <main class="h-218 bg-gray-500 text-white pt-20">
+    <center>
+      <div>
+        BIG Problems?<br />
+        Meet BIGGER solutions
+      </div>
+    </center>
+    <div>
+      Join the chain of thinkers, doers, and innovators, solving problems
+      together<br />
+      Post your challenge, find your expert, and unlock solutions.
+      <br />Collaboration made effortless.
+    </div>
+    <div><button>Get Started</button><button>How It Works</button></div>
+    <div>
+      <video width="440" height="160" loop autoplay muted>
+        <source src="../assets/cube.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   </main>
 </template>
