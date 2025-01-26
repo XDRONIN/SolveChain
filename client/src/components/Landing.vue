1096601821b06516d433ffe07f58050d23631cba
XDRONIN <akashprakashmattathil@gmail.com>
2025-01-26 22:16:09 +0530

    how it works button

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -48,10 +48,16 @@
 
         <div class="flex flex-row justify-center pt-2">
           <button
-            class="relative flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-fuchsia-300 to-fuchsia-800 px-7 py-3 rounded-full border border-black hover:scale-105 duration-200 hover:text-white hover:border-gray-800 hover:from-fuchsia-800 hover:to-fuchsia-400 before:absolute before:top-[-2px] before:left-[-2px] before:right-[-2px] before:bottom-[-2px] before:rounded-full before:bg-gradient-to-r before:from-fuchsia-400 before:to-fuchsia-800 before:blur-md before:opacity-50 before:z-[-1]"
+            class="relative flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-fuchsia-300 to-fuchsia-800 px-7 py-3 rounded-full border border-black hover:scale-105 duration-200 hover:text-white hover:border-gray-800 hover:from-fuchsia-800 hover:to-fuchsia-400 before:absolute before:top-[-2px] before:left-[-2px] before:right-[-2px] before:bottom-[1px] before:rounded-full before:bg-gradient-to-r before:from-fuchsia-400 before:to-fuchsia-800 before:blur-md before:opacity-50 before:z-[-1]"
           >
-            Get Started ></button
-          ><button>How It Works</button>
+            Get Started >
+          </button>
+          <div class="w-6"></div>
+          <button
+            class="relative flex gap-3 cursor-pointer text-fuchsia-800 font-semibold bg-black px-7 py-3 rounded-full border-2 border-fuchsia-800 hover:scale-105 duration-200 hover:text-white hover: to-fuchsia-800 hover:from-fuchsia-800 hover:to-fuchsia-200 before:absolute before:top-[1px] before:left-[1px] before:right-[1px] before:bottom-[1px] before:rounded-full before:bg-gradient-to-r before:from-fuchsia-400 before:to-fuchsia-800 before:blur-md before:opacity-50 before:z-[-1]"
+          >
+            How It Works
+          </button>
         </div>
       </center>
       <div
