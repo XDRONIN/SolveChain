ab0ce439a5e3c699b234ab0fec4de2e0cb4bae4c
XDRONIN <akashprakashmattathil@gmail.com>
2025-04-20 13:09:12 +0530

    admin Dashboard initialized

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Landing from "../views/Landing.vue";
 import SignUp from "../views/SignUp.vue";
 import Dashboard from "../views/Dashboard.vue";
+import AdminDash from "../views/admin/AdminDash.vue";
 
 const routes = [
   { path: "/", component: Landing },
   { path: "/signUp", component: SignUp },
   { path: "/dashboard", component: Dashboard },
+  { path: "/adminDashboard", component: AdminDash },
 ];
 
 const router = createRouter({
---
+++ client/src/views/admin/AdminDash.vue
@@ -0,0 +1,12 @@
+<!-- src/views/Admin/Dashboard.vue -->
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold mb-6">Admin Dashboard</h1>
+
+    <div class="mt-10">
+      <h2 class="text-2xl font-semibold mb-4">Recent Users</h2>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
